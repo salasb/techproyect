d5ea55b2701b7a290c9e11739cb6193b851eaf62
Chris <salasb@gmail.com>
2026-02-04 21:12:30 -0300

    Borrar archivo duplicado

--- src/app/(dashboard)/page.tsx
+++
@@ -1,7 +0,0 @@
-
-// This file is disabled to avoid conflict with src/app/page.tsx
-// The dashboard route is now at /dashboard/page.tsx
-
-// export default async function UnusedDashboard() {
-//    return null; 
-// }
